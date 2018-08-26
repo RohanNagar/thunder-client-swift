baa118cde1e8214224b00ccc3c6097e364bce79e
Nick Eckert <n.eckert70@gmail.com>
2018-08-26 17:59:14 -0500

    Initial Commit

---
+++ ThunderClientSwift/ThunderClientSwift/ThunderClientSwift.swift
@@ -0,0 +1,11 @@
+//
+//  ThunderClientSwift.swift
+//  ThunderClientSwift
+//
+//  Created by Nick Eckert on 8/26/18.
+//  Copyright © 2018 Sanction. All rights reserved.
+//
+
+class ThunderClientSwift {
+
+}
